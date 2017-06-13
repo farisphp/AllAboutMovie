38d060fef078f0e5cd025f1a33149c036eed6481
farisphp <faris.perwira@gmail.com>
2017-06-13 17:26:08 +0700

    moviedetail

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl415/allaboutmovie/Service/API.java
@@ -0,0 +1,6 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl415.allaboutmovie.Service;
+
+public class API {
+    static public String API_URL = "http://api.themoviedb.org/3/discover/movie";
+    static public String API_KEY = "acd6ae11a54a8400ecf0b31e75ae9d8f";
+}
